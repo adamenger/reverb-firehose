9b4bbca6d10d3888e9c183c4bef997dff0a9194b
Adam Enger <adamenger@gmail.com>
2015-04-29 13:49:13 -0500

    updating to 50 listings

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var app = angular.module("Firehose", []);
 var polling_rate = 3000;
+var per_page = 50;
 
 app.controller("ListingCtrl", function($scope, $http, $timeout) {
   var poll = function () {
     $timeout(function(){
-      $http.get('https://reverb.com/api/listings.json').
+      $http.get('https://reverb.com/api/listings.json?per_page=' + per_page).
         success(function(data, status, headers, config) {
           $scope.listings = data['listings'];
         }).
